db3cb22ccb10a7437dc08292b486103f6a8a1284
AmanSamaiya <amansamaiya90@gmail.com>
2022-11-16 14:57:24 +0530

    bcrypt encryption

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
  const bodyParser = require("body-parser");
  const ejs = require("ejs");
  const mongoose = require("mongoose");
- const encrypt = require("mongoose-encryption");
-
+ const bcrypt = require("bcrypt");
+ const saltRounds = 10;
  const app = express();
 
  app.use(express.static("public"));
@@ -19,10 +19,6 @@
    email:String,
    password:String
  });
-
-
-
-userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:['password']});
 
  const User = new mongoose.model("User",userSchema);
 
@@ -40,19 +36,22 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:req.body.password
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
 
-  newUser.save(function(err){
-    if(err){
-     console.log(err);
-    }
-    else{
-        res.render("secrets");
-    }
-  });
+    newUser.save(function(err){
+      if(err){
+       console.log(err);
+      }
+      else{
+          res.render("secrets");
+      }
+    });
+});
+
 });
 
 app.post("/login",function(req,res){
@@ -64,9 +63,12 @@ app.post("/login",function(req,res){
     }
     else{
       if(foundUser){
-        if(foundUser.password===password){
+        bcrypt.compare(password,foundUser.password, function(err, result) {
+          if(result===true){
             res.render("secrets");
-        }
+          }
+
+       })
       }
     }
   })
